16b7c8de5bbed0013725d3978aaf5426ed4252e7
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-10-10 16:44:51 +0200

    initial branch commit

--- obj/Debug/net9.0/dnd_game.AssemblyInfo.cs
+++ obj/Debug/net9.0/dnd_game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dnd_game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f6c1dd49c32f17a0b693cc0594cf4a60fd438d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4420ad030ee815d4c0e458fcd86670504d297fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dnd_game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dnd_game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
