364dd4fef456ccb06cedadb655d7beb8e8494558
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-11-13 11:35:48 +0100

    changelog too big to fit into 1 commit, check desc
    - Split sorting code from CheckInventory() to SortInventory()
    - add Loot() from player class for future compatability
    - expanded TakeTurn() for simple NPC behaviour and battle options for
      player

--- Entity.cs
+++ Entity.cs
@@ -21,6 +21,44 @@ abstract class Entity
         InventorySize = inventorySize;
         Inventory = new Item[InventorySize];
     }
+    public void SortInventory()
+    {
+        // Puts all items to beginning of array
+        Item[] temp = new Item[InventorySize];
+        foreach (Item? item in Inventory)
+        {
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] == null)
+                {
+                    if (item != null)
+                    { temp[i] = item; }
+                    break;
+                }
+            }
+        }
+        Array.Clear(Inventory, 0, Inventory.Length);
+        Inventory = temp;
+    }
+    public virtual void CheckInventory(bool equip = false)
+    {
+        string txt = "--Your Inventory--\n";
+
+        // Checking inventory: 
+        for (int i = 0; i < InventorySize; i++)
+        {
+            txt += $"[{i + 1}] [{Inventory.ElementAtOrDefault(i)?.Name ?? "Empty Slot"}]\n";
+        }
+        Console.WriteLine(txt);
+    }
+    public virtual string Info()
+    {
+        string txt = "___________________\n";
+
+        txt += $"Name: [{Name}]\n"
+             + $"HP:   [{Hp}]\n";
+        return txt;
+    }
     public int InventoryRange()
     {
         int amount = 0;
@@ -49,52 +87,7 @@ abstract class Entity
             }
         }
     }
-    public void ReceiveItem(List<Item> items)
-    {
-        string txt = "Loot:\n";
-        txt += "________________\n";
-        for (int i = 0; i < items.Count; i++)
-        {
-            txt += $"[{i}] '{items[i]}'\n";
-        }
-        txt += "________________ ";
-        string? input = "";
-        while (input!.ToLower() != "exit")
-        {
-            try{Console.Clear();} catch{}
-            Utility.Prompt(txt);
-            input = Console.ReadLine();
-            int.TryParse(input, out int output);
-            AddItem(items[output]);
-        }
-    }
-    public List<Item> TransferItem(Item item)
-    {
-        List<Item> items = new List<Item>();
-        for (int i = 0; i < InventorySize; i++)
-        {
-            if (Inventory[i] != null)
-            {
-                items.Add(Inventory[i]!);
-                Inventory[i] = null;
-            }
-        }
-        return items;
-    }
-    public void DiscardItem(Item item)
-    {
-        for (int i = 0; i < InventorySize; i++)
-        {
-            if (Inventory[i] != null)
-            {
-                if (item == Inventory[i])
-                {
-                    Inventory[i] = null;
-                    break;
-                }
-            }
-        }
-    }
+    public virtual void Loot(Entity victim) {}
     public void TakeDamage(Entity enemy)
     {
         double dmg = 0;
@@ -116,13 +109,56 @@ abstract class Entity
                 dmg = w.CritCheck(dmg);
             }
         }
-        if (this is Player) { Utility.PrintColor("You took " + dmg + " damage!", ConsoleColor.DarkRed); }
-        else { Utility.PrintColor(this.Name + " took " + dmg + " damage!", ConsoleColor.DarkGreen); }
+        if (this is Player) { Utility.PrintColor("\nYou took " + dmg + " damage!", ConsoleColor.DarkRed); }
+        else { Utility.PrintColor("\n" + this.Name + " took " + dmg + " damage!", ConsoleColor.DarkGreen); }
         this.Hp -= dmg;
-        if (this.Hp <= 0) {this.Alive = false;}
+        if (this.Hp <= 0) { this.Alive = false; }
+        Console.ReadKey(true);
     }
     public virtual void TakeTurn(Entity opponent)
     {
-        
+        bool subRunning = true;
+        int selectedIndex = 0;
+        while (subRunning)
+        {
+            Console.Clear();
+            Utility.GenerateMenu("BATTLE\n" + this.Name + "'s turn...");
+            if (this is Player)
+            {
+                string[] battleOptions = ["Attack", "Pass"];
+                Utility.PrintColor(opponent.Info(), ConsoleColor.DarkRed);
+                Utility.PrintColor(this.Info(), ConsoleColor.DarkGreen);
+                Utility.GenerateMenuActions(selectedIndex, battleOptions);
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedIndex--;
+                        if (selectedIndex < 0)
+                            selectedIndex = battleOptions.Length - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedIndex++;
+                        if (selectedIndex > battleOptions.Length - 1)
+                            selectedIndex = 0;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (battleOptions[selectedIndex] == "Attack")
+                        {
+                            opponent.TakeDamage(this);
+                            subRunning = false;
+                        }
+                        else if (battleOptions[selectedIndex] == "Pass") 
+                        {subRunning = false;}
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine(this.Name + " swings at you!");
+                opponent.TakeDamage(this);
+                subRunning = false;
+            }
+        }
     }
 }
